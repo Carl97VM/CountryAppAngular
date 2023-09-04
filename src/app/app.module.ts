bd8436dc83e04662e6b6cbd3153551f6c5bd1a65
Carl97VM <barcacarlosv@gmail.com>
2023-09-04 13:45:19 -0400

    Creacion de rutas para el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRountingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    AppRountingModule,
+
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'share-contact-page',
+  templateUrl: './home-page.component.html',
+  styles: [
+  ]
+})
+export class HomePageComponent {
+
+}
